51c52288556ededcfc83e46c93fe378d0c6a2e24
Aleks Seltenreich <aleks@blendwerk.net>
2020-11-01 13:36:02 +0100

    Added stof/doctrine-extensions-bundle dependency

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true]
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
