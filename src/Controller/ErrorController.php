39d2aabe97de733abeb92e1d43332cc1b578aa12
Aleks Seltenreich <aleks@blendwerk.net>
2020-11-26 17:48:21 +0100

    Added /500 and /404 error routes

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ErrorController extends BaseController
+{
+    /**
+     * @Route("/500", name="error_500")
+     */
+    public function internalServerError(): void
+    {
+        throw new HttpException(500);
+    }
+
+    /**
+     * @Route("/404", name="error_404")
+     */
+    public function pageNotFound(): void
+    {
+        throw new NotFoundHttpException();
+    }
+}
